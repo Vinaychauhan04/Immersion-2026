eb780fb5952ef30408f52bb8f28c68ea6ce71d61
vinay <vinaychauhan1539@gmail.com>
2026-06-11 12:24:34 +0530

    Describe the changes you made here

--- java core/helloworld.java
+++ java core/helloworld.java
@@ -1,5 +1,8 @@
-public class HelloWorld {
+public class helloworld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if(System.out.println("Hello, World!") != null) {
+            
+        }
     }
 }
